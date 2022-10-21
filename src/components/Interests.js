38b08fd0d8df7ea6f88a5f4005c29e721a9e0dcd
eunhachingu <espajunarjr@gmail.com>
2022-10-22 01:28:20 +0800

    first_project

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Interests() {
     return(
         <div className="body">
-            <h3 className="interests">Interests</h3>
+            <h3 className="title">Interests</h3>
             <p className="body-text">
                 Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
             </p>
