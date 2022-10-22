71aeee6547724e63b169c01baaf4ae17a4ba1a37
eunhachingu <espajunarjr@gmail.com>
2022-10-22 14:25:23 +0800

    second section

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Info from './components/Info';
-import About from './components/About';
-import Interests from './components/Interests';
-import Footer from './components/Footer';
+import Jokes from './components/Jokes';
+
 
 function App() {
     return (
         <div className='container'>
-            <Info />
-            <About />
-            <Interests />
-            <Footer />
+            <Jokes 
+                setup = "I got my daughter a fridge for her birthday."
+                punchline = "I can't wait to see her face light up when she opens it."
+            />
+
+            <Jokes 
+                setup = "How did the hacker escape the police?"
+                punchline = "He just ransomware!."
+            />
+
+            <Jokes 
+                setup = "Why don't pirates travel on mountain roads?"
+                punchline = "Scurvy."
+            />
+
+            <Jokes 
+                setup = "Why do bees stay in the hive in the winter?"
+                punchline = "Swarm."
+            />
+
+            <Jokes 
+                setup = "What's the best thing about Switzerland?"
+                punchline = "I don't know, but the flag is a big plus!"
+            />
         </div>
     )
 }
