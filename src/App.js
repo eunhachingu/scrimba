b37ef497e95ab4182f1b5018c589bec2cd45e4bb
eunhachingu <espajunarjr@gmail.com>
2022-10-23 11:38:34 +0800

    airbnb

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Jokes from './components/Jokes';
+import Card from './components/Card';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 
 
 function App() {
     return (
         <div className='container'>
-            <Jokes 
-                setup = "I got my daughter a fridge for her birthday."
-                punchline = "I can't wait to see her face light up when she opens it."
+            <Navbar />
+            <Hero />
+            <Card 
+                img="katie.png"
+                rating="5.0"
+                reviewCount={6}
+                country="America"
+                title="Life Lessons with Katie Zaferes"
+                price={136}
             />
 
-            <Jokes 
-                setup = "How did the hacker escape the police?"
-                punchline = "He just ransomware!."
-            />
-
-            <Jokes 
-                setup = "Why don't pirates travel on mountain roads?"
-                punchline = "Scurvy."
-            />
-
-            <Jokes 
-                setup = "Why do bees stay in the hive in the winter?"
-                punchline = "Swarm."
-            />
-
-            <Jokes 
-                setup = "What's the best thing about Switzerland?"
-                punchline = "I don't know, but the flag is a big plus!"
-            />
         </div>
     )
 }
