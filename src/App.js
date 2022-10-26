d6f5557a1bd2e93787235e13c0aa019a4846a213
eunhachingu <espajunarjr@gmail.com>
2022-10-26 20:45:06 +0800

    main3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Info from './components/Info';
-import About from './components/About';
-import Interests from './components/Interests';
-import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Meme from './components/Meme';
 
 function App() {
     return (
         <div className='container'>
-            <Info />
-            <About />
-            <Interests />
-            <Footer />
+            <Navbar />
+            <Meme />
         </div>
     )
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+        <img src="../images/trollFace.png" alt="troll face" className='nav--logo'/>
+        <h1 className='nav--title'>Meme Generator</h1>
+        <p className='nav--project'>React Course - Project 3</p>
+    </nav>
+  )
+}
+
+export default Navbar
